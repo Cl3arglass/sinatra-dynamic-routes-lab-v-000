658ef57d3321502808bf64192db4d4839ff9cf80
Holden Altaffer <haltaffer@gmail.com>
2018-06-15 00:53:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     "#{@phrases.join(" ")}"
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
   @sentence = params.values.join(" ")
    "#{@sentence}."
   end
