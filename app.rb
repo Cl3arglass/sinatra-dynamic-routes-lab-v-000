93e0b5ccb4fd794d178da92d76618d5194e0be1f
Holden Altaffer <haltaffer@gmail.com>
2018-06-15 01:04:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,8 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    "#{params[:number1].to_i.send(params[:operation], params[:number2].to_i)}"
+    @solution = params[:number1].to_i.send(params[:operation], params[:number2].to_i)
+    "#{@solution}"
   end
 
 
