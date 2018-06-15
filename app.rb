3244e355fac70d906ec80ae14ffdc73a414d0c63
Holden Altaffer <haltaffer@gmail.com>
2018-06-15 00:32:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    "#{params[:number].times {puts params[:phrase]}}"
+    "#{params[:number].times {params[:phrase]}}"
   end
 
 
