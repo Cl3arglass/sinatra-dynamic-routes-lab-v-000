be81e2a233343cc52d171e0b63b819799a4621f0
Holden Altaffer <haltaffer@gmail.com>
2018-06-15 00:27:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-
-  "#{params[:name].reverse}"
+   "#{params[:name].reverse}"
   end
+
+  get '/square/:number' do
+    "#{params[:number].to_i ** 2}"
+  end
+
+
 
 
 end
